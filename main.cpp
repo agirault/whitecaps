16045741862874e757e6afdcbfb94324f7ca5281
Alexis Girault <girault.al@gmail.com>
2014-12-17 20:22:30 -0500

    BUG : CORRECT OCEAN POSITION PROBLEM

--- main.cpp
+++ main.cpp
@@ -1958,8 +1958,8 @@ int main(int argc, char* argv[]) {
         glBindTexture(GL_TEXTURE_2D, textures[TEXTURE_OCEAN_POSITION]);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F,FFT_SIZE, FFT_SIZE, 0, GL_RGBA, GL_FLOAT, NULL);
 
 /**/
